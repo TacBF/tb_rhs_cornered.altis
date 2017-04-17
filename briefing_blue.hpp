4cb6c06a3b9014add0bf3b0f8bf90e38842f5dbb
Ballista Milsim <ballista.milsim@gmail.com>
2017-04-17 16:38:05 +0200

    tb_rhs_cornered.altis mission added

---
+++ briefing_blue.hpp
@@ -0,0 +1,10 @@
+// Provide side specific mission briefing here. Text can use structured text syntax. Use <br /> tag for line breaks, plus all other Arma tags.
+<font size='14' color='#FFA500'>Deployment Options:</font><br />
+* You can get to the AO either by spawning at the RP's  or by going from the main base Car and APCs.<br />
+
+<font size='14' color='#FFA500'>Defense Strategies:</font><br />
+* Utilize your Heavy MWS Supplies at flags (Domes and Barracks).<br />
+    -- Heavy MWS = TOW, Mk19.<br />
+* MSW attachable on offroads.<br />
+* Establish FO's quickly.<br />
+* Establish RP's quickly.<br />
---
+++ briefing_red.hpp
@@ -0,0 +1,9 @@
+// Provide side specific mission briefing here. Text can use structured text syntax. Use <br /> tag for line breaks, plus all other Arma tags.
+<font size='14' color='#FFA500'>Deployment Options:</font><br />
+* You can get to the AO going from the main base by Car and APCs.<br<br />
+
+
+<font size='14' color='#FFA500'>Attack Strategies:</font><br />
+* Utilize your MWS Supplies.<br />
+* Establish FO's quickly.<br />
+* Establish RP's quickly.<br />
